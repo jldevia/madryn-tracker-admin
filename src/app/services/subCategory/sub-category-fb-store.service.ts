4b0b2b219c35a4ccf8442496b3e3e8b1a2ea786d
jldevia <jldevia81@gmail.com>
2021-08-02 22:41:29 -0300

    Cambios parciales. Integracion con Firebase.

--- src/app/services/subCategory/sub-category-fb-store.service.ts
+++ src/app/services/subCategory/sub-category-fb-store.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { SubCategory } from 'src/app/models/subCategory';
+import { LoadingService } from '../loading.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class SubCategoryFBStoreService {
+	private subCategories: SubCategory[] = [];
+	private subCategories$: BehaviorSubject<SubCategory[]> = new BehaviorSubject<SubCategory[]>([]);
 
-  constructor() { }
+	get subCategoriesObservable(): Observable<SubCategory[]> {
+		return this.subCategories$.asObservable();
+	}
+
+	constructor(private fireStore: AngularFirestore, private loadingService: LoadingService) {}
+
+  private getAllSubcategories(): Observable<SubCategory[]> {
+    this.loadingService.showLoading();
+
+    
+  }
 }
